797ebeb0bd0090ed7edc5f05bd4afaeef5cb6849
studio-kakky <info@studio-kakky.com>
2022-08-02 17:06:49 +0900

    scssインストール

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,7 +1,8 @@
 import { JSX } from "preact"
+import styles from './index.module.scss'
 
 export const CommonHeader = (): JSX.Element => {
     return (
-        <div>header2</div>
+        <div className={styles.aa}><p>header2</p></div>
     )
 }
